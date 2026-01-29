0e2d5e34f4ab34f570a19b6c66d6983ff30e9e66
Pushina_TL <Pushina_TL@directum.ru>
2026-01-29 13:19:17 +0400

    Обновление на 25.3

--- GD.Fias/GD.Fias.Server/ModuleAsyncHandlers.cs
+++ GD.Fias/GD.Fias.Server/ModuleAsyncHandlers.cs
@@ -6,7 +6,7 @@ using Sungero.CoreEntities;
 
 namespace GD.Fias.Server
 {
-  public class ModuleAsyncHandlers
+  public partial class ModuleAsyncHandlers
   {
 
   }
--- GD.FiasSolution/GD.FiasSolution.Server/ModuleServerFunctions.cs
+++ GD.FiasSolution/GD.FiasSolution.Server/ModuleServerFunctions.cs
@@ -6,7 +6,7 @@ using Sungero.CoreEntities;
 
 namespace GD.FiasSolution.Server
 {
-  public class ModuleFunctions
+  public partial class ModuleFunctions
   {
 
   }
